8881ffc76259f7538e227ed688471ddb7540277f
Sreevathsa <sreevathsagowru@gmail.com>
2021-01-27 00:13:44 +0530

    Home Page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,10 @@ function HomePage() {
   return (
     <div className="home-container">
       <Link to="/expense-tracker">
-        <button class="btn blue">Expense Tracker</button>
+        <button class="btn yellow">Expense Tracker</button>
+      </Link>
+      <Link to="/calculator">
+        <button class="btn blue">Calculator</button>
       </Link>
     </div>
   );
