78a2a556493d663a41d6de8038e863cd4889ca9f
Sreevathsa <sreevathsagowru@gmail.com>
2021-01-27 01:05:37 +0530

    Color Codes - Input

--- src/ColorCodes/components/Input.js
+++ src/ColorCodes/components/Input.js
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Input.css";
 
 function Input(props) {
+  const [Hex, setHex] = useState("");
+  const [Rgb, setRgb] = useState("");
+  const [Hsl, setHsl] = useState("");
+
+  useEffect(() => {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+  }, [Hex]);
+
+  const changeColor = (e) => {
+    let target = e.target.placeholder;
+
+    if (target === "HEX") {
+      setHex(e.target.value);
+    } else if (target === "RGB") {
+      setRgb(e.target.value);
+    } else {
+      setHex(e.target.value);
+    }
+  };
+
   return (
     <div className="color-input-container">
-      <input className="color-input" type="text" placeholder="HEX" />
-      <input className="color-input" type="text" placeholder="RGB" />
-      <input className="color-input" type="text" placeholder="HSL" />
+      <input
+        className="color-input"
+        value={Hex}
+        // onChange={(e) => setHex(e.target.value)}
+        onChange={changeColor}
+        type="text"
+        placeholder="HEX"
+      />
+      <input
+        className="color-input"
+        value={Rgb}
+        onChange={changeColor}
+        // onChange={(e) => setRgb(e.target.value)}
+        type="text"
+        placeholder="RGB"
+      />
+      <input
+        className="color-input"
+        value={Hsl}
+        // onChange={(e) => setHsl(e.target.value)}
+        onChange={changeColor}
+        type="text"
+        placeholder="HSL"
+      />
     </div>
   );
 }
