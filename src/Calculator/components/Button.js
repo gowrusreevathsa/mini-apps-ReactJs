945e0a5303bad1479fccfae77bf74cc5be1f0f9f
Sreevathsa <sreevathsagowru@gmail.com>
2021-01-26 13:30:32 +0530

    Calculator Basic Css

--- src/Calculator/components/Button.js
+++ src/Calculator/components/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Button.css";
 
+const isOperator = (val) => {
+  return !isNaN(val) || (val === ".") | (val === "=");
+};
+
 function Button(props) {
-  return <div>{props.children}</div>;
+  return <div className="button">{props.children}</div>;
 }
 
 export default Button;
